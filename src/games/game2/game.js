8ba9d3460c8fe966e320fb0ad2765bc2a34374ea
PabloMera6 <pabmergom@alum.us.es>
2024-01-08 13:07:52 +0100

    Arreglos

--- src/games/game2/game.js
+++ src/games/game2/game.js
@@ -2,26 +2,34 @@ export class Game extends Phaser.Scene {
     constructor() {
         super({ key: 'game' });
     }
-  
+
     preload() {
         this.load.image('redLight', 'assets/images/globo-bueno.png');
     }
-  
+
     create() {
         this.attempts = 3;
         this.currentAttempt = 0;
         this.bestTime = Number.MAX_SAFE_INTEGER;
+
+        // Elemento invisible para bloquear el input durante ciertos momentos
         this.inputGuard = this.add.rectangle(-100, -100, 1, 1);
         this.inputGuard.visible = false;
         this.inputGuard.setInteractive();
+
+        // Texto para mostrar el intento actual
         this.attemptText = this.add.text(400, 50, '', {
             fontSize: '24px',
             fill: '#fff',
         }).setOrigin(0.5);
+
+        // Texto para mostrar el tiempo de reacción
         this.reactionTimeText = this.add.text(400, 200, '', {
             fontSize: '24px',
             fill: '#fff',
         }).setOrigin(0.5);
+
+        // Texto para el siguiente intento
         this.nextAttemptText = this.add.text(400, 400, '', {
             fontSize: '20px',
             fill: '#fff',
@@ -29,13 +37,18 @@ export class Game extends Phaser.Scene {
         this.nextAttemptText.setInteractive();
         this.nextAttemptText.on('pointerdown', this.continueGame, this);
         this.nextAttemptText.setVisible(false);
+
+        // Comienza el primer intento
         this.startNewAttempt();
     }
-  
+
     createLights() {
+        // Si el grupo de luces no existe, lo crea
         if (!this.lightsGroup) {
             this.lightsGroup = this.add.group();
         }
+
+        // Inicializa el evento para encender las luces uno por uno
         for (let i = 0; i < 4; i++) {
             this.time.delayedCall(i * 1000, () => {
                 const light = this.lightsGroup.create(100 + i * 150, 300, 'redLight');
@@ -43,118 +56,120 @@ export class Game extends Phaser.Scene {
                 light.setVisible(true);
             });
         }
+
         // Usa el método once en lugar de on
-        this.input.once('pointerdown', this.handleEarlyClick, this);
+        this.input.on('pointerdown', this.handleEarlyClick, this);
+
+        // Inicializa el evento para desaparecer los globos después de un tiempo aleatorio
+        // Guarda el evento en una variable
         this.hideEvent = this.time.delayedCall(5000, () => {
             this.lightsGroup.children.iterate((light) => {
                 this.lightsGroup.killAndHide(light);
             });
-  
+
             this.startTime = this.time.now;
-  
-            this.nextAttemptText.setText('Realizar el siguiente intento');
-            this.nextAttemptText.setVisible(true);
-  
+
             this.input.off('pointerdown', this.handleEarlyClick, this);
-  
+
             this.input.on('pointerdown', this.handleReactionClick, this);
         });
     }
-    
+
     handleEarlyClick() {
-        this.reactionTimeText.setText(`Has perdido un intento`);
-        this.reactionTimeText.setPosition(400, 150);
+        // Bloquea el input y muestra el mensaje de error
+        this.inputGuard.visible = true;
         this.lightsGroup.clear(true, true);
         this.calculateReactionTime(2000);
         this.reactionTimeText.setText(`Tiempo de Reacción: 2000 ms`);
         this.reactionTimeText.setPosition(400, 200);
-        this.nextAttemptText.setText('Realizar el siguiente intento');
-        this.nextAttemptText.setVisible(true);
-        this.nextAttemptText.setPosition(400, 400);
         this.input.off('pointerdown', this.handleEarlyClick, this);
-        // Elimina la línea que resta un intento
-        // this.currentAttempt--;  
-        this.input.disable(this.inputGuard);
-        this.hideEvent.remove();
-     
+        this.input.off('pointerdown', this.handleReactionClick, this);
+        // Detiene el evento hideEvent si está programado para ejecutarse
+        if (this.hideEvent) {
+            this.hideEvent.remove();
+        }
+        // Elimina todos los globos que existan actualmente
+        if (this.lightsGroup) {
+            this.lightsGroup.clear(true, true);
+        
+        }
+        
+
+
         // Limpia los globos y prepara el próximo intento
-        this.lightsGroup.children.each(function(child){
+        this.lightsGroup.children.each(function (child) {
             child.destroy();
         });
-        
+
+        // Muestra la pantalla intermedia
         this.showIntermediateScreen();
     }
-   
+
     showIntermediateScreen() {
-        // Muestra un mensaje de error
-        this.errorMessage = this.add.text(400, 300, '¡Clic demasiado pronto! Intenta de nuevo.', {
-            fontSize: '24px',
-            fill: '#fff',
-        }).setOrigin(0.5);
-     
+
         // Crea un botón para comenzar el próximo intento
-        this.nextButton = this.add.text(400, 400, 'Continuar', {
+        this.nextButton = this.add.text(400, 400, 'Realizar siguiente intento', {
             fontSize: '24px',
             fill: '#fff',
         }).setOrigin(0.5).setInteractive();
-     
+
         // Cuando el jugador hace clic en el botón, oculta el mensaje de error y comienza el próximo intento
         this.nextButton.on('pointerdown', () => {
-            this.errorMessage.destroy();
             this.nextButton.destroy();
-            this.startNewAttempt();
+            this.inputGuard.visible = false;  // Desbloquea el input
+            this.continueGame();
         }, this);
-     }
-     
-    
+    }
+
     handleReactionClick() {
         const reactionTime = this.calculateReactionTime();
         this.reactionTimeText.setText(`Tiempo de Reacción: ${reactionTime} ms`);
-        this.nextAttemptText.setText('Realizar el siguiente intento');
-        this.nextAttemptText.setVisible(true);
-    
+
         this.input.off('pointerdown', this.handleReactionClick, this);
-    
+
         if (reactionTime < this.bestTime) {
             this.bestTime = reactionTime;
         }
-    
+
         this.lightsGroup.clear(true, true);
+        this.showIntermediateScreen();
+
     }
-    
-    
+
     calculateReactionTime(time) {
-          if (time) {
-              return time;
-          }
-          else{
-              return this.time.now - this.startTime;
-          }
+        if (time) {
+            return time;
+        } else {
+            return this.time.now - this.startTime;
+        }
     }
-    
+
     continueGame() {
         this.reactionTimeText.setText('');
         this.nextAttemptText.setVisible(false);
-        this.input.enable(this.inputGuard);
+        this.inputGuard.visible = false;  // Desbloquea el input
         this.startNewAttempt();
     }
-    
+
     startNewAttempt() {
         this.currentAttempt++;
+
+        // Muestra el intento actual
         this.attemptText.setText(`Intento ${this.currentAttempt}/${this.attempts}`);
         this.reactionTimeText.setText('');
-    
+
+        // Comienza un nuevo intento si hay intentos restantes
         if (this.currentAttempt <= this.attempts) {
             this.createLights();
         } else {
             this.endGame();
         }
     }
-  
+
     endGame() {
         // Oculta el texto del intento actual
         this.attemptText.setVisible(false);
-  
+
         // Muestra el mejor tiempo al final del juego
         if (this.bestTime !== Number.MAX_SAFE_INTEGER) {
             this.add.text(400, 300, `Mejor Tiempo: ${this.bestTime}ms`, {
@@ -167,9 +182,8 @@ export class Game extends Phaser.Scene {
                 fill: '#fff',
             }).setOrigin(0.5);
         }
-  
+
         // Destruye el grupo de luces
         this.lightsGroup.clear(true, true);
     }
-  }
-  
+}
